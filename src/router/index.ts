16fcd8b6289df1ac8e60c7a526d1f0b0e47502b2
Lilith Silver <84940819+LilithSilver@users.noreply.github.com>
2026-04-08 00:24:11 -0700

    better scrolling

--- src/router/index.ts
+++ src/router/index.ts
@@ -29,6 +29,13 @@ function generateDocsRoutes(docs: Doc[]) {
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  },
   routes: [
     {
       path: '/',
